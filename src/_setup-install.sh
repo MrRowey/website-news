b5399b1083a249ea265d72a57d4178e553ce994a
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-01-28 11:30:56 +0100

    Add bash scripts to install and serve the project (#9)

---
+++ src/_setup-install.sh
@@ -0,0 +1,9 @@
+# Retrieves the packages required for this project 
+#
+# See also: 
+# - https://bundler.io/
+# - https://bundler.io/v2.6/man/bundle-install.1.html
+
+bundle install
+
+read
---
+++ src/_setup-serve.sh
@@ -0,0 +1,10 @@
+# Serves the website locally 
+# 
+# See also: 
+# - https://bundler.io/
+# - https://bundler.io/v2.6/man/bundle-exec.1.html
+# - https://jekyllrb.com/docs/usage/
+
+bundle exec jekyll serve
+
+read
